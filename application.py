f77fe006f99211f2008611673d469399c449281a
itpazu <&;/39euJaA##xZ/>
2020-07-12 15:28:20 +0300

    -"with yml"

--- application.py
+++ application.py
@@ -1,30 +1,78 @@
-from flask import Flask, json
+from flask import Flask, json, request
 import pymongo
 from flask_cors import CORS
 import os
 from db.Data_Layer import DataLayer
 from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
+from decouple import config
+from functools import wraps
+
 
 
 load_dotenv()
 application = Flask(__name__)
 CORS(application)
 bcrypt = Bcrypt(application)
-__client = pymongo.MongoClient('10.150.54.176:27017', 27017, username=os.getenv("USER_NAME"),
-                               password=os.getenv("PASSWORD"))
-
+__client = pymongo.MongoClient('10.150.54.176:27017', 27017, username=config("USER_NAME"),
+                               password=config("PASSWORD"))
 dataLayer = DataLayer(bcrypt, __client)
 
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        try:
+
+            content = request.json
+            user_id = content['user_id']
+            token = request.headers.get('auth-token')
+            authenticate = dataLayer.authenticate_user(user_id, token)
+
+        except Exception as err:
+            response = application.response_class(
+                response=json.dumps({"error": str(err)}),
+                status=401,
+                mimetype='application/json'
+            )
+            return response
+
+        return f(*args, **kwargs)
+
+    return decorated
+
 @application.route('/')
+@token_required
 def say_hello():
     return 'HELLO KEEPER HOME', 200, {"Content-Type": "application/json"}
 
 @application.route('/get_doc/<string:user_name>')
 def get_doc(user_name):
-    user_dict = dataLayer.get_doc(user_name)
+    user_dict = dataLayer.get_doc_by_user_name(user_name)
     resp = json.dumps(user_dict, default=str), 200, {"Content-Type": "application/json"}
     return resp
+
+
+@application.route('/login')
+def log_in():
+
+    try:
+        content = request.json
+
+        user_name = content['username']
+        password = content['password']
+
+        if not user_name or not password:
+            raise ValueError('error: missing information')
+
+        execute_login = dataLayer.log_user(user_name, password)
+        return json.dumps(execute_login, default=str), 200, {"Content-Type": "application/json"}
+
+    except Exception as error:
+        return json.dumps(error, default=str), 401, {"Content-Type": "application/json"}
+
+
+
 
 if __name__ == "__main__":
     port = os.environ.get('PORT')
--- db/Data_Layer.py
+++ db/Data_Layer.py
@@ -1,16 +1,24 @@
-from flask import request
+from flask import request, json
 from models.user import User
+from Util import decode_token, encode_token
+import jwt
 
 class DataLayer:
 
-    def get_doc(self, user_name):
+    def get_doc_by_user_name(self, user_name):
+
         user_dict = self.__db.Users.find_one({"username": user_name})
         if user_dict:
             return user_dict
         else:
-            resp = 'No such user name exists!'
-        return resp
+            return None
 
+    def get_doc_by_user_id(self, user_id):
+        user_dict = self.__db.Users.find_one({"user_id": user_id})
+        if user_dict:
+            return user_dict
+        else:
+            return None
 
     def add_user(self):
         first_name = request.get_json()['first_name']
@@ -19,7 +27,7 @@ class DataLayer:
         email = request.get_json()['email']
         admin = request.get_json()['admin']
         user_id = request.get_json()['user_id']
-        password = self.bcrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
+        password = self.encrypt_pass(request.get_json()['password'])
         if self.__db.Users.find_one({"email": email}):
             added_user = {'status': 'The email is already in the system!'}
         elif self.__db.Users.find_one({"user_id": user_id}):
@@ -32,7 +40,67 @@ class DataLayer:
             added_user = {'status': 'The user has been added!'}
         return added_user
 
+    def log_user(self, user_name, password):
 
+        verify_user_exsits = self.get_doc_by_user_name(user_name)
+
+        if verify_user_exsits is None:
+            raise ValueError('email does not exist in db')
+
+        else:
+            db_password = verify_user_exsits["password"]
+            # print(db_password)
+            compare_pass = self.match_password(db_password, password)
+
+            if compare_pass:
+                user_id = str(verify_user_exsits['user_id'])
+                generated_token = encode_token(user_id, db_password)
+                store_token = self.store_token(user_id, generated_token)
+                get_user_dict = self.get_doc_by_user_id(user_id)
+                return get_user_dict
+
+            raise ValueError('password is incorrect')
+
+    def authenticate_user(self, user_id, token):
+
+        user_from_db = self.get_doc_by_user_id(user_id)
+        if user_from_db is None:
+            raise ValueError('identification failed, user_id is either missing or incorrect')
+        pass_from_db = user_from_db['password']
+        decoded_token = decode_token(token, user_id, pass_from_db)
+
+        try:
+            if user_id != decoded_token:
+                raise ValueError('Invalid session, please log in again')
+            return True
+        except jwt.ExpiredSignatureError:
+            raise ValueError('Signature expired. Please log in again.')
+        except jwt.InvalidTokenError:
+            raise ValueError('Invalid token. Please log in again.')
+
+        except ValueError as error:
+            raise error
+
+
+
+
+    def decrypt_pass(self, password):
+        pass
+
+    def encrypt_pass(self, password):
+        return self.bcrypt.generate_password_hash(password).decode('utf-8')
+
+
+    def match_password(self, db_pass, recieved_password):
+
+        if self.bcrypt.check_password_hash(db_pass, recieved_password):
+            return True
+        else:
+            return False
+
+    def store_token(self, user_id, token):
+        store_token = self.__db.Users.update({"user_id": user_id}, {"$set": {"token": token}})
+        return store_token
 
     def __init__(self, bcrypt, client):
 
