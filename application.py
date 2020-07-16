c7205223574c60e0464d30c42ed50bf48a5c012d
itpazu <&;/39euJaA##xZ/>
2020-07-16 15:59:26 +0300

    fixes master

--- application.py
+++ application.py
@@ -177,10 +177,11 @@ def change_password(user_id):
 
 def _build_cors_preflight_response():
     response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    response.headers.add('Access-Control-Allow-Credentials', True)
+    response.headers("Access-Control-Allow-Origin", "*")
+    response.headers('Access-Control-Allow-Headers', "*")
+    response.headers('Access-Control-Allow-Methods', "*")
+    response.headers('Access-Control-Allow-Credentials', 'true')
+
     return response
 
 
